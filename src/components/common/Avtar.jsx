b5ef223df9259b25fa84389629e1de8cb66a09c2
Ayush Saxena <71811059+AyushSaxena01@users.noreply.github.com>
2023-11-18 03:53:16 +0530

    added post, avtar components

--- src/components/common/Avtar.jsx
+++ src/components/common/Avtar.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import PropTypes from 'prop-types';
 
-export default function Avtar({imgUrl, userName, userHandle}) {
+export default function Avtar({imgUrl, userName, userHandle,size='m'}) {
 
     Avtar.propTypes={
-        imgUrl:PropTypes.string.isRequired
+        imgUrl:PropTypes.string.isRequired,
+        size:PropTypes.string.isRequired
     }
+
+   const Size = {
+    s:'w-9 h-9',
+    m:'w-12 h-12',
+    l:''
+   }
 
   return (
     <>
      <div className="flex flex-row items-start gap-x-4 self-stretch">
-        <button className="w-12 h-12 flex-shrink-0 rounded-6xl">
-          <img className="rounded-full" src={imgUrl} alt="Avtar" />
-        </button>
+        
+          <img className={`${Size[size]} rounded-full shrink-0`} src={imgUrl} alt="Avtar" />
+        
         {
-             (typeof(userHandle) !== undefined && typeof(userName) !== undefined) && (
+             (userHandle !== undefined && userName !== undefined) && (
                 <button className="flex flex-row items-start gap-x-2 self-stretch">
                 <span className="font-Inter text-base not-italic font-medium leading-1.2 text-neutral/50">{userName}</span>
                 <span className="font-Inter text-base not-italic font-normal text-neutral/500">{userHandle} •
