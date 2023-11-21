6b86ee4467e27cb237bdb775119df66f9689bb29
Ayush Saxena <71811059+AyushSaxena01@users.noreply.github.com>
2023-11-22 04:26:47 +0530

    Added context api & routes  to onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,72 @@
+import { AuthProvider } from './context/AuthProvider';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Login from "./pages/Login";
+import Step1 from './pages/Step1';
+import Step2 from './pages/Step2';
+import Step3 from './pages/Step3';
+import Step4 from './pages/Step4';
+import UserProfile from './pages/UserProfile'
+import EditProfile from './pages/EditProfile';
+import ComposeTweet from './pages/ComposeTweet';
+import HomeFeed from './pages/HomeFeed';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />,
+  },
+  {
+    path: "/composetweet",
+    element: <ComposeTweet />,
+  },
+  {
+    path: "/Step1",
+    element: <Step1 />,
+  },
+  {
+    path: "/Step2",
+    element: <Step2 />,
+  },
+  {
+    path: "/Step3",
+    element: <Step3 />,
+  },
+  {
+    path: "/Step4",
+    element: <Step4 />,
+  },
+  {
+    path: "/HomeFeed",
+    element: <HomeFeed /> ,
+  },
+  {
+    path: "/UserProfile",
+    element: <UserProfile />,
+  },
+  {
+    path: "/EditProfile",
+    element: <EditProfile /> ,
+  },
+]);
+
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+
+  return (
+  <>
+<AuthProvider>
+  {/* <TwitterApp /> */}
+  <RouterProvider router={router} />
+</AuthProvider>
+  </>
+
+   )
 }
 
 export default App;
+
+
+
